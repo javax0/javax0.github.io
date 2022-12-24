dee4ec075ef5153ce735cd67237947dbc3bbf553
Peter Verhas <peter@verhas.com>
2022-12-24 06:45:45 +0100

    article in progress

--- _projects/2022-12-13-hidden-classes/src/test/java/com/javax0/blog/hiddenclasses/TestHiddenClassLoader.java
+++ _projects/2022-12-13-hidden-classes/src/test/java/com/javax0/blog/hiddenclasses/TestHiddenClassLoader.java
@@ -4,18 +4,19 @@ import com.javax0.sourcebuddy.Compiler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.lang.invoke.MethodHandles;
 
+// snipline TestHiddenClassLoader filter=(Test\w+)
 public class TestHiddenClassLoader {
 
-    public interface Hello {
+    // snippet InterfaceHello
+    interface Hello {
         void hello();
     }
-
-    public static byte[] load(final String name, final String code) throws Compiler.CompileException, IllegalAccessException {
-        final var classByteArray = Compiler.java().from(name, code).compile().stream().toArray(byte[][]::new);
-        return classByteArray[0];
-    }
+    // end snippet
 
     // snippet CODE1
     private static final String CODE1 = """
@@ -36,17 +37,55 @@ public class TestHiddenClassLoader {
     // snipline CLASS_NAME
     public static final String CLASS_NAME = "com.javax0.blog.hiddenclasses.MySpecialClass";
 
+    private static PrintStream out;
+    private static void setout(final String output) throws FileNotFoundException {
+        out = System.out;
+        System.setOut(new PrintStream(output));
+    }
+    private static void resetout(){
+        System.setOut(out);
+    }
+
     @Test
     @DisplayName("Invoke the hidden class and say hello")
     void sayHello() throws Exception {
+        setout("hello_parameters.txt");
         // snippet sayHello
-        final var byteCode = load(CLASS_NAME, CODE1);
+        final var byteCode = Compiler.java().from(CODE1).compile().get();
         final var lookup = MethodHandles.lookup();
-        final var classLookup =
-                lookup.defineHiddenClass(byteCode, true, MethodHandles.Lookup.ClassOption.NESTMATE);
+        final var classLookup = lookup.defineHiddenClass(byteCode, true);
         final var helloClass = (Class<Hello>) classLookup.lookupClass();
 
-        helloClass.getConstructor().newInstance().hello();
+        final var hello = helloClass.getConstructor().newInstance();
+        hello.hello();
+        //end snippet
+        // snippet hidden_parameters
+        System.out.println(hello.getClass());
+        System.out.println(hello.getClass().getSimpleName());
+        System.out.println(hello.getClass().getName());
+        System.out.println(hello.getClass().getCanonicalName());
+        // end snippet
+        resetout();
+    }
+
+    @Test
+    @DisplayName("Load a hidden class initialized vs. one not initialized")
+    void initVsNonInit() throws Exception {
+        // snippet initVsNonInit
+        final var CODE =
+                """
+                        package com.javax0.blog.hiddenclasses;
+                        public class A {
+                            static {
+                              System.out.println("Class A is initialized");
+                            }
+                        }
+                        """;
+        final var byteCodeA = Compiler.java().from(CODE).compile().get();
+        final var byteCodeB = Compiler.java().from(CODE.replaceAll("A","B")).compile().get();
+        final var lookup = MethodHandles.lookup();
+        lookup.defineHiddenClass(byteCodeA, true);
+        lookup.defineHiddenClass(byteCodeB, false);
         //end snippet
     }
 
@@ -54,8 +93,8 @@ public class TestHiddenClassLoader {
     @DisplayName("Invoke multiple hidden classes and say hello")
     void sayHelloTwice() throws Exception {
         // snippet sayHelloTwice
-        final var byteCode1 = load(CLASS_NAME, CODE1);
-        final var byteCode2 = load(CLASS_NAME, CODE2);
+        final var byteCode1 = Compiler.java().from(CODE1).compile().get();
+        final var byteCode2 = Compiler.java().from(CODE2).compile().get();
         final var lookup1 = MethodHandles.lookup();
         final var classLookup1 =
                 lookup1.defineHiddenClass(byteCode1, true, MethodHandles.Lookup.ClassOption.NESTMATE);
@@ -77,7 +116,7 @@ public class TestHiddenClassLoader {
     @DisplayName("Invoke the hidden class and say hello using SourceBuddy")
     void sayHellotoSB() throws Exception {
         // snippet sayHelloSB
-        final var hello = Compiler.java().from(CLASS_NAME, CODE1).compile().loadHidden().newInstance(CLASS_NAME, Hello.class);
+        final var hello = Compiler.java().from(CLASS_NAME, CODE1).hidden().compile().load().newInstance(CLASS_NAME, Hello.class);
         hello.hello();
         //end snippet
     }
